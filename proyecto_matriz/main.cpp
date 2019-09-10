acb09a8ad9393596618ccc04e8c105223cace39f
Oteranga <40015080+Oteranga@users.noreply.github.com>
2019-09-09 20:30:22 -0500

    Update main.cpp

--- proyecto_matriz/main.cpp
+++ proyecto_matriz/main.cpp
@@ -6,6 +6,13 @@ using namespace std;
 
 int main()
 {
+    Matriz ma(5,5);
+    ma.llenar();
+    ma.escalar(4);
+    ma.trasposicion();
+    ma.mostrar();
 
+    Matriz ma2(200,200);
+    ma.mostrar();
     return 0;
 }
